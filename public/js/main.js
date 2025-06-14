88923b5a29957d4241f8b80d18f1edb2b20ee48a
Vlad-736 <shevtsov736@gmail.com>
2025-06-14 15:16:06 +0300

    commit2

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", async () => {
-  // рисуем кнопки
-  const fetchButtonsData = await import("./components/fetchButtonsData.js");
-  const renderBtn = await import("./components/renderBtn.js");
-  fetchButtonsData.default("../data/btn.json").then((fetchButtonsData) => {
-    renderBtn.default(fetchButtonsData);
-  });
-});

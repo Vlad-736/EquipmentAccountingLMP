5c022243daf46dc70e12a0f2a95a39f110eaea15
Vlad-736 <shevtsov736@gmail.com>
2025-06-15 20:50:57 +0300

    Update choiseLocation.js

--- public/js/components/choiseLocation.js
+++ public/js/components/choiseLocation.js
@@ -2,14 +2,24 @@ export default function choiseLocation() {
   const btnChoise = document.querySelector('.location__shop');
   const locationShopName = document.querySelector('.location__shop-name');
   const locationShopChoise = document.querySelectorAll(".location__sublink");
-  btnChoise.addEventListener('click', function () {
+
+  const savedLocation = localStorage.getItem('selectedLocation');
+  if (savedLocation) {
+    locationShopName.textContent = savedLocation;
+  }
+
+  btnChoise.addEventListener('click', function (e) {
+    e.preventDefault()
     btnChoise.classList.toggle("location__shop--active")
   });
 
-  locationShopChoise.forEach(element => {
-    element.addEventListener('click', function () {
-      btnChoise.classList.toggle("location__shop--active")
-      locationShopName.textContent = element.textContent
+   locationShopChoise.forEach(element => {
+    element.addEventListener('click', function (event) {
+      event.stopPropagation();
+      const selectedText = element.textContent;
+      locationShopName.textContent = selectedText;
+      localStorage.setItem('selectedLocation', selectedText); // сохранить в localStorage
+      btnChoise.classList.remove("location__shop--active");
     });
   });
 

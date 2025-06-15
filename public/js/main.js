259ea0ef2e6464c18e59daae0f37a108b917dad5
Vlad-736 <shevtsov736@gmail.com>
2025-06-15 20:52:50 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,11 @@
 window.addEventListener("DOMContentLoaded", async () => {
-  // рисуем кнопки
+  const choiseLocation = await import("./components/choiseLocation.js");
+  choiseLocation.default();
   const fetchButtonsData = await import("./components/fetchButtonsData.js");
   const renderBtnType = await import("./components/renderBtnType.js");
   fetchButtonsData.default("./btn.json").then((fetchButtonsData) => {
     renderBtnType.default(fetchButtonsData);
   });
-
   const btnBack = await import("./components/btnBack.js");
   btnBack.default();
-
-  const choiseLocation = await import("./components/choiseLocation.js");
-  choiseLocation.default();
 });
